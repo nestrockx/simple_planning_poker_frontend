caf5ff716868b42ce706cc57c1bfd1f0a35bf13b
nestrockx <wegielek.art@gmail.com>
2025-05-29 18:46:20 +0200

    update urls

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://simple-planning-poker-backend.onrender.com/api',
+  baseURL: '/api/api',
   withCredentials: true,
 })
 
--- src/api/request.tsx
+++ src/api/request.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-  baseURL: 'https://simple-planning-poker-backend.onrender.com',
+  baseURL: '/api',
   withCredentials: true,
 })
 
